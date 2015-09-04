d3fe3c4bea7f8f08db158349ccd53dcca6d6248f
Lance Zhu <lancezhu77@gmail.com>
2015-09-03 22:31:04 -0700

    Refactor.

--- Pod/Classes/AssetsKit+extension.swift
+++ Pod/Classes/AssetsKit+extension.swift
@@ -10,7 +10,8 @@ import Foundation
 
 extension AssetsKit {
     
-    internal class func x_imageOfEtcIcon(sizeToFit: CGSize?, drawingSize: CGSize, backgroundColor: UIColor? = nil, opaque: Bool? = true, imageContextScale: CGFloat? = 0) -> UIImage {
+    private class func drawing(draw: (scaledX: CGFloat, scaledY: CGFloat) -> Void, sizeToFit: CGSize?, drawingSize: CGSize, backgroundColor: UIColor? = nil, opaque: Bool? = true, imageContextScale: CGFloat? = 0) -> UIImage {
+        
         // enter the size of the original drawing
         
         let rect = CGRect(origin: CGPointZero, size: sizeToFit ?? drawingSize)
@@ -20,15 +21,30 @@ extension AssetsKit {
             backgroundColor.set()
             UIRectFill(rect)
         }
-
+        
         // calculate scaleX and scaleY based on size
         let scaledX: CGFloat = (sizeToFit ?? drawingSize).width / drawingSize.width
         let scaledY: CGFloat = (sizeToFit ?? drawingSize).height / drawingSize.height
-        AssetsKit.drawEtcIcon(scaleX: scaledX, scaleY: scaledY)
+        
+        draw(scaledX: scaledX, scaledY: scaledY)
         
         let imageOfEtcIcon = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         
         return imageOfEtcIcon
+    }
+    
+    internal class func x_imageOfEtcIcon(sizeToFit: CGSize?, drawingSize: CGSize, backgroundColor: UIColor? = nil, opaque: Bool? = true, imageContextScale: CGFloat? = 0) -> UIImage {
+        
+        return self.drawing(
+            { scaledX, scaledY in
+                AssetsKit.drawEtcIcon(scaleX: scaledX, scaleY: scaledY)
+            },
+            sizeToFit: sizeToFit,
+            drawingSize: drawingSize,
+            backgroundColor: backgroundColor,
+            opaque: opaque,
+            imageContextScale: imageContextScale
+        )
     }
 }

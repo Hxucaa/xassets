2fb1044d87c8477892d2258c3296ab0d7eedd6df
Connor Wang <cwang@lai.io>
2015-10-11 03:07:57 -0700

    Added Treat and WTG Icons for the pressed state

--- Pod/Classes/AssetsKit+extension.swift
+++ Pod/Classes/AssetsKit+extension.swift
@@ -439,7 +439,7 @@ extension AssetsKit {
                 let scaledX: CGFloat = (sizeToFit ?? drawingSize).width / drawingSize.width
                 let scaledY: CGFloat = (sizeToFit ?? drawingSize).height / drawingSize.height
                 // change this function to the appropriate drawing function
-                AssetsKit.drawTreatIcon(scaleX: scaledX, scaleY: scaledY)
+                AssetsKit.drawTreatIcon(scaleX: scaledX, scaleY: scaledY, ifPressed: false)
             },
             
             sizeToFit: sizeToFit,
@@ -460,7 +460,7 @@ extension AssetsKit {
                 let scaledX: CGFloat = (sizeToFit ?? drawingSize).width / drawingSize.width
                 let scaledY: CGFloat = (sizeToFit ?? drawingSize).height / drawingSize.height
                 // change this function to the appropriate drawing function
-                AssetsKit.drawWTGIcon(scaleX: scaledX, scaleY: scaledY)
+                AssetsKit.drawWTGIcon(scaleX: scaledX, scaleY: scaledY, ifPressed: false)
             },
             
             sizeToFit: sizeToFit,
